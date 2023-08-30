51c453986e8ddc70861777baf9fac4620b2bd55a
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-08-30 16:19:19 -0600

    Proyecto completo

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <div className="cont">
+        <h1>Chandelure</h1>
+      </div>
+      <div className="img2">
+          <h1>Eevee</h1>
+      </div>
+      <div className="img3">
+          <h1>Evo</h1>
+      </div>
+      <div className="img4">
+        <h1>Hoenn</h1>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
